102f9511175f9a6d170e043d99db2ce91f0b115d
Matthew Couch <matt.couch@adtran.com>
2020-02-13 12:01:14 -0600

    server register

--- server/src/asterRoyale/asterRoyale.module.ts
+++ server/src/asterRoyale/asterRoyale.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AsterRoyaleGateway } from './asterRoyale.gateway';
+import { AsterRoyaleController } from './asterRoyale.controller';
 
 @Module({
   providers: [AsterRoyaleGateway],
+  controllers: [AsterRoyaleController],
 })
 export class AsterRoyaleModule {}
---
+++ server/src/classes/PlayerData.ts
@@ -0,0 +1,5 @@
+export default class PlayerData {
+  id: number;
+  name: string;
+  score: number = 0;
+}
