1f9ec9b801db8fb6982944883a4adcec169ba41b
Joseph Morris <joseph.morris@adtran.com>
2020-02-13 12:06:53 -0600

    buttons beginning

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import {MatButtonModule} from '@angular/material/button';
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
@@ -5,15 +6,17 @@ import { AppComponent } from "./app.component";
 
 import { SocketIoModule, SocketIoConfig } from "ngx-socket-io";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
+import { FlexLayoutModule } from "@angular/flex-layout";
 const config: SocketIoConfig = { url: "/", options: {} };
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     BrowserAnimationsModule,
-    SocketIoModule.forRoot(config)
+    SocketIoModule.forRoot(config),
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
